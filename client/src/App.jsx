c76bad392a5f92e0435d64f599e55c64cd0cbd5e
SattvaDoshi <sattva103@gmail.com>
2024-10-19 13:06:15 +0530

    Initial Commit

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='font-bold bg-slate-500 p-5 text-white text-center text-3xl'>
+      Data hack
+    </div>
+  )
+}
+
+export default App
