40c6310cf601a3b3fdc44438fe839be96a665659
roushan-jha <jharoushan20@gmail.com>
2024-10-19 23:21:48 +0530

    landing-page/shield

--- client/src/components/FeatureCard.jsx
+++ client/src/components/FeatureCard.jsx
@@ -1,10 +1,10 @@
 
 // eslint-disable-next-line react/prop-types
-const FeatureCard = ({ header, des }) => {
+const FeatureCard = ({ header, image, des }) => {
   return (
     <div className="flex flex-col w-2/3 md:w-1/3 bg-gray-100 bg-opacity-80 text-gray-900 h-[526px] rounded-lg shadow-md">
-        <div className="md:h-2/5">
-            <img src="public\f2.jpg" alt="Image" />
+        <div className="md:h-2/5 bg-yellow-200">
+            <img src={image} alt="Image" />
         </div>
         <div className="h-3/5 flex flex-col justify-center gap-2 p-4">
             <h1 className="font-bold text-2xl">{header}</h1>
--- client/src/components/Features.jsx
+++ client/src/components/Features.jsx
@@ -3,14 +3,17 @@ import FeatureCard from "./FeatureCard"
 const features = [
   {
     header: "Automated Risk Assessment",
+    image: "f1.jpg",
     subpoints: ["Analyzes cybersecurity documentation using AI", "Generates accurate risk scores based on your data", "Provides proactive insights to mitigate vulnerabilities"]
   },
   {
-    header: "Comprehensive Reportingt",
+    header: "Comprehensive Reporting",
+    image: "f2.jpg",
     subpoints: ["Creates detailed, actionable cybersecurity reports", "Identifies key areas of improvement for better security", "Aligns with industry standards like GDPR and HIPAA."]
   },
   {
     header: "Real-time Decision Support",
+    image: "f3.jpg",
     subpoints: ["Offers dynamic, AI-powered recommendations", "Continuously updates risk assessments as data changes", "Enables informed, real-time decision-making for stronger security"]
   },
 ]
@@ -19,9 +22,9 @@ const features = [
 const Features = () => {
   return (
     <div id="feature" className="h-auto md:h-screen bg-gray-100 py-16 bg-opacity-80 my-32 rounded-md flex flex-col md:flex md:flex-row gap-16 md:gap-8 md:px-10 md:justify-around items-center mx-8 md:mx-0">
-      <FeatureCard header={features[0].header} des={features[0].subpoints} />
-      <FeatureCard header={features[1].header} des={features[1].subpoints} />
-      <FeatureCard header={features[2].header} des={features[2].subpoints} />
+      <FeatureCard header={features[0].header} image={features[0].image} des={features[0].subpoints} />
+      <FeatureCard header={features[1].header} image={features[1].image} des={features[1].subpoints} />
+      <FeatureCard header={features[2].header} image={features[2].image} des={features[2].subpoints} />
     </div>
   )
 }
