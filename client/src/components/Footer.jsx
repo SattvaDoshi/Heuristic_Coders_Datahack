a6dfdace83be0d6a1c3b5539713e54b18242a50e
roushan-jha <jharoushan20@gmail.com>
2024-10-19 17:11:48 +0530

    footer and left/hero-section

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,22 @@
+import { Link } from "react-router-dom"
+import { HashLink } from "react-router-hash-link"
 
 const Footer = () => {
   return (
-    <div className="h-24 max-w-7xl m-auto bg-blue-500">Footer</div>
+    <div className="h-24 max-w-7xl m-auto flex justify-between text-gray-200">
+      <div className="flex items-center">
+        <img src="public\shield.png" width={32} alt="Logo" />
+        <h2 className="font-bold text-2xl">SheildAI</h2>
+      </div>
+      <div className="flex flex-col justify-center">
+        <div className="flex justify-end">&copy; by Heuristic Coders</div>
+        <div className="flex gap-4 items-center">
+          <Link to="/">Home</Link>
+          <HashLink to="/#feature">Features</HashLink>
+          <Link to="/assesment">Upload</Link>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,7 +1,13 @@
 
 const Hero = () => {
   return (
-    <div className="h-screen">Hero</div>
+    <div className="h-screen flex gap-4 items-center text-white">
+      <div className="flex flex-col gap-4 w-3/5">
+        <h2 className="font-semibold text-5xl w-4/5 leading-tight">Transform Your Cybersecurity with ShieldAI</h2>
+        <p className="text-xl w-3/4 leading-7">Use ShieldAI's generative AI to effortlessly analyze cybersecurity documentation, generating detailed reports and risk scores to enhance your defenses and inform your decisions.</p>
+      </div>
+      <div className="w-2/5">Right components</div>
+    </div>
   )
 }
 
